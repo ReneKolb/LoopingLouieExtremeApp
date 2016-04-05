f2e533713ce299d530c8945b8edd24cc636e3dba
Admi <rene.kolb@gmail.com>
2016-04-05 13:23:30 +0200

    forgot to use the new main menu layout :o

--- app/src/main/java/de/renekolb/loopinglouieextreme/ui/MainMenuFragment.java
+++ app/src/main/java/de/renekolb/loopinglouieextreme/ui/MainMenuFragment.java
@@ -61,7 +61,7 @@ public class MainMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_menu_test, container, false);
 
         tvProfileName = (TextView) view.findViewById(R.id.tv_main_menu_profile_name);
         tvProfileName.setText(fa.getCurrentPlayer().getPlayerName());
